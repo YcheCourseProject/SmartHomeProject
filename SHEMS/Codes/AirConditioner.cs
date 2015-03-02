54a269ce60c2c88729c9f74045475e476cb89cb8
CheYulin <191836400@qq.com>
2015-03-02 21:08:47 +0800

    CommitV1
    Codes下文件是类文件，

---
+++ SHEMS/Codes/AirConditioner.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Windows.Storage.Streams;
+using Windows.Networking.Sockets;
+using Windows.Networking;
+
+
+namespace SHEMS.entities
+{
+    class AirConditioner
+    {
+        static int temperature = 21;
+        static AC_MODE ac_mode = AC_MODE.WARM;
+        static string HOST_IP = "10.0.0.151";
+        static string SEVER_PORT = "60000";
+        static byte AC_CODE_ON = (byte)0x55;
+        static byte AC_CODE_OFF = (byte)0x80;
+      
+        public enum AC_MODE
+        {
+            COLD, WARM, VENTILATION, DEHYDRATION
+        }
+        //必须要每次重新连套接字，否则的话要考虑连接中断的问题
+        private static async void setACSettings(byte sendbyte)
+        {
+            StreamSocket clientSocket = null;
+            try
+            {
+                HostName serverHost = new HostName(HOST_IP);
+                clientSocket = new StreamSocket();
+                await clientSocket.ConnectAsync(serverHost, SEVER_PORT);
+                DataWriter writer = new DataWriter(clientSocket.OutputStream);
+                writer.WriteByte(sendbyte);
+                await writer.StoreAsync();
+                writer.DetachStream();
+                writer.Dispose();
+            }
+            catch
+            {
+                clientSocket.Dispose();
+                clientSocket = null;
+            }
+        }
+       
+        public static void downTemperature()
+        {
+            if (temperature > 16)
+            {
+                temperature--;
+                setACTemperatureMode(temperature, ac_mode);
+            }
+        }
+        public static void upTemperature()
+        {
+            if (temperature < 30)
+            {
+                temperature++;
+                setACTemperatureMode(temperature, ac_mode);
+            }
+        }
+        public static void setACTemperatureMode(int temperature, AC_MODE mode)
+        {
+            byte temp;
+            switch (mode)
+            {
+                case AC_MODE.COLD:
+                    temp = (byte)0x40;
+                    break;
+                case AC_MODE.WARM:
+                    temp = (byte)0x50;
+                    break;
+                case AC_MODE.VENTILATION:
+                    temp = (byte)0x60;
+                    break;
+                case AC_MODE.DEHYDRATION:
+                    temp = (byte)0x70;
+                    break;
+                default:
+                    temp = (byte)0x40;
+                    break;
+            }
+            if (temperature < 16 || temperature > 30)
+                temperature = 21;
+            temp = (byte)(temp + (temperature - 16));
+            setACSettings(temp);
+        }
+        public static void onAC()
+        {
+            setACSettings(AC_CODE_ON);
+        }
+        public static void OffAC()
+        {
+            setACSettings(AC_CODE_OFF);
+        }
+    }
+}
---
+++ SHEMS/MainPage.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+//线程相关
+using System.Threading;
+using System.Threading.Tasks;
+using SHEMS.entities;
+
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Networking.Sockets;
+using Windows.Networking;
+using Windows.Storage.Streams;
+
+
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace SHEMS
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    ///  
+
+    public sealed partial class MainPage : Page
+    {
+        //上下文
+        SynchronizationContext context;
+        bool acqflag = true;
+        bool isReadyOn = true;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            context = SynchronizationContext.Current;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+            Task.Factory.StartNew(() =>
+            {
+                ThreadProcAcqTmpHumid();
+            });
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame frame = Window.Current.Content as Frame;
+
+            frame.Navigate(typeof(BlankPage1));
+        }
+
+
+        public void ThreadProcOnOffAC(bool isReadyOn)
+        {
+            context.Post(async (s) =>
+            {
+
+                //可以在此访问UI线程中的对象，因为代理本身是在UI线程的上下文中执行的  
+                if (isReadyOn == true)
+                    AirConditioner.onAC();
+                else
+                    AirConditioner.OffAC();
+                // MessageDialog messageDialog = new MessageDialog("ThreadProc1");
+                //await messageDialog.ShowAsync();
+            }, null);
+        }
+        public void ThreadProcUpDownAC(bool isReadyDown)
+        {
+            context.Post(async (s) =>
+            {
+
+                //可以在此访问UI线程中的对象，因为代理本身是在UI线程的上下文中执行的  
+                if (isReadyDown == true)
+                    AirConditioner.downTemperature();
+                else
+                    AirConditioner.upTemperature();
+            }, null);
+        }
+        public void ThreadProcAcqTmpHumid()
+        {
+
+            context.Post(async (s) =>
+            {
+
+                //可以在此访问UI线程中的对象，因为代理本身是在UI线程的上下文中执行的  
+                StreamSocket clientSocket = new StreamSocket();
+                try
+                {
+                    HostName serverHost = new HostName(TmpHumidCtrl.TMP_HUM_SERVER_IP);
+                    await clientSocket.ConnectAsync(serverHost, TmpHumidCtrl.TMP_HUM_PORT);
+                    String sb = "";
+                    String temprature;
+                    String humidity;
+                    DataReader reader = new DataReader(clientSocket.InputStream);
+                    reader.InputStreamOptions = InputStreamOptions.Partial;  //采用异步方式
+                    await reader.LoadAsync(1024);  //获取一定大小的数据流
+                    int tempint;
+
+                    while (acqflag)
+                    {
+                        tempint = reader.ReadByte();
+                        if (tempint == '%')
+                        {
+                            char[] ch = new char[] { ':' };
+                            String[] tempstrs = sb.Split(ch);
+                            string tempstr = tempstrs[1];
+                            temprature = tempstr.Substring(2, 6);
+                            humidity = tempstrs[1].Substring(9, 13);
+                            TextBox_Tmp.Text = temprature;
+                            TextBox_Humid.Text = humidity;
+                            sb = "";
+                        }
+                        else
+                        {
+                            sb = sb + (char)tempint;
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                    Debug.WriteLine(e.StackTrace);
+                    clientSocket.Dispose();
+                    clientSocket = null;
+                }
+                // MessageDialog messageDialog = new MessageDialog("ThreadProc1");
+                //await messageDialog.ShowAsync();
+            }, null);
+        }
+        public void ThreadProcOnOffSW(bool isReadyOn)
+        {
+            context.Post(async (s) =>
+            {
+
+                //可以在此访问UI线程中的对象，因为代理本身是在UI线程的上下文中执行的  
+                if (isReadyOn == true)
+                    for (int i = 0; i < 255; i++)
+                    {
+                        SwitchCtrl.switchOn(SwitchCtrl.SW_SERVER_IP + i);
+                    }
+
+                else
+                    for (int i = 0; i < 255; i++)
+                    {
+                        SwitchCtrl.switchOff(SwitchCtrl.SW_SERVER_IP + i);
+                    }
+            }, null);
+        }
+        private void Button_Click_OnAC(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                ThreadProcOnOffAC(true);
+            });
+        }
+
+        private void Button_Click_OffAC(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                ThreadProcOnOffAC(false);
+            });
+        }
+
+        private void Button_Click_TmpDown(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                ThreadProcUpDownAC(true);
+            });
+        }
+
+        private void Button_Click_TmpUp(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                ThreadProcUpDownAC(false);
+            });
+        }
+
+        private void Button_Click_SW(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                ThreadProcOnOffSW(isReadyOn);
+                isReadyOn = !isReadyOn;
+            });
+        }
+
+
+
+    }
+}
